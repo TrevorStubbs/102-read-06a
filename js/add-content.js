8db13567ce357bbd8f6d7229ffc4cf01f175e5a2
Trevor Stubbs <stubbste@gmail.com>
2020-04-15 14:59:36 -0700

    update

--- js/add-content.js
+++ js/add-content.js
@@ -1,1 +1,16 @@
 // add JS content below
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+
+if (hourNow>18){
+    greeting = 'Good evening!';
+} else if(hourNow >12) {
+    greeting = 'Good afternoon!';    
+} else if (hourNow >0) {
+    greeting = 'Welcome!';
+} else {
+    greeting = 'Welcome!';
+}
+
+document.write('<h3>' + greeting + '</h3>');
